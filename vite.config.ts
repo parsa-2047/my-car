5d080e2cfade9ec57ec6c39c18d2e1b27805271d
parsa-2047 <147753509+parsa-2047@users.noreply.github.com>
2026-07-20 17:31:35 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -5,7 +6,41 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+  react(),
+  tailwindcss(),
+  VitePWA({
+    registerType: "autoUpdate",
+
+    includeAssets: ["favicon.ico"],
+
+    manifest: {
+      name: "My Car",
+      short_name: "MyCar",
+      description: "Offline Car Assistant",
+      theme_color: "#2563eb",
+      background_color: "#ffffff",
+      display: "standalone",
+      start_url: "/",
+      icons: [
+        {
+          src: "icon-192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "icon-512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
+    },
+
+    workbox: {
+      globPatterns: ["**/*.{js,css,html,png,svg,ico}"]
+    }
+  })
+],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
